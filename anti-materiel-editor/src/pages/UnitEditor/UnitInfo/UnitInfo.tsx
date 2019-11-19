2cc7e9cfab31bff8a73354da945f5bc09c85bc50
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2019-11-19 11:01:49 -0600

    moved ToastHub from master page to app

--- anti-materiel-editor/src/pages/UnitEditor/UnitInfo/UnitInfo.tsx
+++ anti-materiel-editor/src/pages/UnitEditor/UnitInfo/UnitInfo.tsx
@@ -1,44 +1,12 @@
 import React from 'react';
 import { ManagedContent } from '../../../components/ManagedContent/ManagedContent';
 import { UnitInfoForm } from './UnitInfoForm';
-import { Button } from '../../../components/Button/Button';
-import { useToast } from '../../../components/Toasts/useToast';
 
 export const UnitInfo: React.FC = () => {
-  const makeToast = useToast();
   return (
     <ManagedContent
-      warn
       title="Unit Info"
-      content={() => (
-        <>
-          <Button
-            onClick={() =>
-              makeToast(`Everyday I'm toasting more and more and more...`)
-            }
-          >
-            Toast
-          </Button>
-          <Button
-            onClick={() =>
-              makeToast(`Everyday I'm toasting`, {
-                color: 'warn',
-              })
-            }
-          >
-            Toast
-          </Button>
-          <Button
-            onClick={() =>
-              makeToast(`Everyday I'm toasting`, {
-                color: 'danger',
-              })
-            }
-          >
-            Toast
-          </Button>
-        </>
-      )}
+      content={() => null}
       form={(hideSideBar, onCancel) => (
         <UnitInfoForm
           closeSideDrawer={hideSideBar}
