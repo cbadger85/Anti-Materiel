962d0fe9e6ac03484e053cac93f069a480aaa6dc
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2019-11-19 11:23:09 -0600

    increased time on toast ⏱

--- anti-materiel-editor/src/components/Toasts/createToast.ts
+++ anti-materiel-editor/src/components/Toasts/createToast.ts
@@ -3,7 +3,7 @@ import { ToastOptions, Toast } from './toastsTypes';
 
 export const defaultOptions: Required<ToastOptions> = {
   color: 'info',
-  duration: 500,
+  duration: 700,
 };
 
 export const createToast = (text: string, options?: ToastOptions): Toast => ({
