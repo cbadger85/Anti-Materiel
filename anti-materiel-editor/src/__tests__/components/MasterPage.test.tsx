5e4c4abb61c4d901e79034ae5d70c4fe64baeea3
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2019-11-20 13:54:55 -0600

    :white_check_mark: added tests for MasterPage

--- anti-materiel-editor/src/__tests__/components/MasterPage.test.tsx
+++ anti-materiel-editor/src/__tests__/components/MasterPage.test.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { MasterPage } from '../../components/MasterPage/MasterPage';
 
 // TODO write tests for this
 
 describe('<MasterPage />', () => {
-  it('should have tests', () => {
-    test.todo;
+  it('Shows the provided side panel', () => {
+    const SidePanel = jest.fn();
+
+    const wrapper = shallow(
+      <MasterPage
+        pageTitle="test"
+        mainContent={jest.fn}
+        sidePanelContent={SidePanel}
+      />,
+    );
+
+    expect(SidePanel).toHaveBeenCalled();
+  });
+
+  it('Shows the provided main content', () => {
+    const Main = jest.fn();
+
+    const wrapper = shallow(
+      <MasterPage
+        pageTitle="test"
+        mainContent={jest.fn}
+        sidePanelContent={Main}
+      />,
+    );
+
+    expect(Main).toHaveBeenCalled();
   });
 });
