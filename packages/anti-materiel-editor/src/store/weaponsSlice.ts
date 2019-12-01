e70827229fa72cd1b8e38078cc4ee5dc76792fd2
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2019-12-01 08:52:08 -0600

    :construction: added weaponsSlice

---
+++ packages/anti-materiel-editor/src/store/weaponsSlice.ts
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Weapon } from '@anti-materiel/types';
+
+const initialState: Weapon[] = [];
+
+const weaponsSlice = createSlice({
+  name: 'weapons',
+  initialState,
+  reducers: {
+    addWeapon(state, action: PayloadAction<Weapon>) {
+      state.push(action.payload);
+    },
+    updateWeapon(state, action: PayloadAction<Weapon>) {
+      const index = state.findIndex(weapon => weapon.id === action.payload.id);
+
+      if (index >= 0) {
+        state[index] = action.payload;
+      }
+    },
+    removeWeapon(state, action: PayloadAction<{ id: string }>) {
+      return state.filter(w => w.id !== action.payload.id);
+    },
+  },
+});
+
+export const { addWeapon, updateWeapon, removeWeapon } = weaponsSlice.actions;
+
+export default weaponsSlice.reducer;
