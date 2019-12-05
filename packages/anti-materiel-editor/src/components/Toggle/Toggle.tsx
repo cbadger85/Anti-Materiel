d90f4d20384622ebb16190d27155ee05aaa64665
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2019-12-05 09:58:49 -0600

    :lipstick: style toggle component

--- packages/anti-materiel-editor/src/components/Toggle/Toggle.tsx
+++ packages/anti-materiel-editor/src/components/Toggle/Toggle.tsx
@@ -1,22 +1,47 @@
 import React from 'react';
+import uuid from 'uuid/v4';
 import './Toggle.scss';
 
-export const Toggle: React.FC<ToggleProps> = ({ label, checked, onChange }) => {
+export const Toggle: React.FC<ToggleProps> = ({
+  label,
+  id,
+  name,
+  checked,
+  onChange,
+}) => {
+  const getId = (): string => {
+    if (id) {
+      return id;
+    }
+
+    if (name) {
+      return name;
+    }
+
+    return uuid();
+  };
+
+  const toggleId = getId();
+
   return (
-    <>
+    <div className="toggle">
       <input
-        type="checked"
-        className="toggle"
+        type="checkbox"
         checked={checked}
         onChange={onChange}
+        id={toggleId}
       />
-      <label>{label}</label>
-    </>
+      <label className="toggle__label" htmlFor={toggleId}>
+        {label}
+      </label>
+    </div>
   );
 };
 
 interface ToggleProps {
   label: string;
+  id?: string;
+  name?: string;
   checked?: boolean;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
--- packages/anti-materiel-editor/src/pages/Index/index.tsx
+++ packages/anti-materiel-editor/src/pages/Index/index.tsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SinglePaneLayout } from '../../components/Layouts/SinglePaneLayout';
+import { Toggle } from '../../components/Toggle/Toggle';
 
 export const Index: React.FC = () => {
-  return <SinglePaneLayout title="Editor" mainContent={() => null} />;
+  const [toggle, setToggle] = useState(false);
+  return (
+    <SinglePaneLayout
+      title="Editor"
+      mainContent={() => (
+        <Toggle
+          label="toggle component"
+          checked={toggle}
+          onChange={e => setToggle(e.target.checked)}
+        />
+      )}
+    />
+  );
 };
