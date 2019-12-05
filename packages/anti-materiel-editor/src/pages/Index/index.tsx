3a2f7f11b6c39f625c0e438e45f0d831abb3acf9
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2019-12-05 11:37:47 -0600

    :pencil: remove test component

--- packages/anti-materiel-editor/src/pages/Index/index.tsx
+++ packages/anti-materiel-editor/src/pages/Index/index.tsx
@@ -4,17 +4,5 @@ import { Toggle } from '../../components/Toggle/Toggle';
 
 export const Index: React.FC = () => {
   const [toggle, setToggle] = useState(false);
-  return (
-    <SinglePaneLayout
-      title="Editor"
-      mainContent={() => (
-        <Toggle
-          label="toggle component"
-          checked={toggle}
-          onChange={value => setToggle(value)}
-          // disabled
-        />
-      )}
-    />
-  );
+  return <SinglePaneLayout title="Editor" mainContent={() => null} />;
 };
