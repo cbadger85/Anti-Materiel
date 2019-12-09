f38913a4af72ba6efe3c4ca48cceadd8642dae78
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2019-12-09 14:52:49 -0600

    :construction: activate all menu links

---
+++ packages/anti-materiel-editor/src/pages/PherowareTacticsEditor/PherowareTacticsEditor.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const PherowareTacticsEditor: React.FC = () => {
+  return null;
+};
---
+++ packages/anti-materiel-editor/src/pages/SpecialRulesEditor/SpecialRulesEditor.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const SpecialRulesEditor: React.FC = () => {
+  return null;
+};
