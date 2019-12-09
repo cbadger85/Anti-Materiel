9e4ba4f32b4483b2353f9d1492c6ecd522e78aee
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2019-12-09 10:18:35 -0600

    :bug: remove flash in header during page transistions

--- packages/anti-materiel-editor/src/components/Header/Header.tsx
+++ packages/anti-materiel-editor/src/components/Header/Header.tsx
@@ -1,35 +1,21 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import antiMaterielLogo from '../../images/anti-materiel.svg';
 import { EditorMenu } from '../EditorMenu/EditorMenu';
 import './Header.scss';
-import antiMaterielLogo from '../../images/anti-materiel.svg';
 
 export const Header: React.FC = () => {
-  const location = useLocation();
-
-  const isActive = location.pathname === '/';
-
   return (
     <>
       <header className="header">
         <div>
-          {isActive ? (
-            <span>
-              <img
-                src={antiMaterielLogo}
-                alt="anti-materiel logo"
-                className="header-logo"
-              />
-            </span>
-          ) : (
-            <Link to="/">
-              <img
-                src={antiMaterielLogo}
-                alt="anti-materiel logo"
-                className="header-logo"
-              />
-            </Link>
-          )}
+          <Link to="/">
+            <img
+              src={antiMaterielLogo}
+              alt="anti-materiel logo"
+              className="header-logo"
+            />
+          </Link>
         </div>
       </header>
       <EditorMenu />
